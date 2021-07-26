51af3cbd771ef1b04c68f37c560ac8a2e5f3f31c
Esther Wavinya <estherwavinya80@gmail.com>
2021-07-26 12:23:42 +0300

    functions

--- Functions/functions.js
+++ Functions/functions.js
@@ -113,6 +113,229 @@ avg(3, 2, 5, 6);     //4
 
 
 
+//greet take 2
+function greet(person) {
+  console.log(`Hi, ${person}!`);
+}
+
+
+greet('Arya');         //"Hi, Arya!"
+greet('Ned');          //"Hi, Ned!"
+
+
+
+
+//
+function greet(nickname) {
+  console.log(`Hi, ${nickname}!`);
+}
+greet('tim');       //Hi, tim!
+
+
+
+
+
+//RollDice function
+function rollDie() {
+  let roll = Math.floor(Math.random() * 6) + 1;
+  console.log(`Rolled: ${roll}`);
+}
+
+function throwDice(numRolls) {
+  for(let i = 0; i < numRolls; i++) {
+    rollDie();
+
+  }
+  
+}
+
+
+throwDice(3);               /*
+Rolled: 4
+Rolled: 6
+Rolled: 2
+*/
+
+throwDice(5);           /*
+Rolled: 6
+Rolled: 4
+Rolled: 6
+Rolled: 2
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+//FUNCTIONS WITH MULTIPLE ARGUMENTS - parameter, argument
+function square(num) {
+  console.log(num * num);
+}
+
+square(4);          //16
+
+
+//multiple parameters
+function sum(x, y) {
+  console.log(x + y);
+}
+
+function divide(a, b) {
+  console.log(a / b);
+}
+
+divide(1, 4);       //0.25
+
+
+divide(4, 1);      //4
+
+
+divide('s', 4);     //NaN
+
+
+divide(5);     //NaN  means 5/undefined
+
+
+
+
+
+
+
+
+//RETURN STATEMENT
+/*
+Built-in methods return values when we call them.
+We can store those values:
+*/
+const yell = "I will end you".toUpperCase();
+yell;             //"I WILL END YOU"
+
+const idx = ['a', 'b', 'c'].indexOf('c');
+idx;             //2
+
+
+
+//NO RETURN
+/*
+Our functions print values out, but do not return anything
+*/
+
+
+function add(x, y) {
+  console.log(x + y);
+}
+//if we try to save it in a variable it becomes undefined
+const sum = add(10, 16);
+sum;       //undefined
+
+add(1, 2);    //3
+
+
+
+
+
+/*
+Using RETURN
+Now we can capture a return value in a variable
+You can only return one thing from a function.
+You can have multiple return statements in a single function.
+*/
+
+function add(x, y) {
+  return x + y;      //RETURN!
+}
+const sum = add(10, 16);
+sum;          //26
+
+const answer = add(100, 200);
+answer;     //300
+
+
+
+
+
+//RETURN
+/*
+The return statement ends function execution AND specifies the value to be returned by that function
+*/
+function square(x) {
+  return x * x;
+  console.log('ALL DONE!');
+}
+
+square(4);       //16
+
+
+//
+function isPurple(color) {
+  if (color.toLowerCase() === 'purple') {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+isPurple('blue');     //false
+isPurple('purple');      //true
+isPurple('PURPLEE');         //false
+isPurple('PURPLE');             //true
+
+
+
+
+//REWRITE PREVIOUS CODE - ELSE IF BY using one return statement
+function isPurple(color) {
+  return color.toLowerCase() === 'purple';
+}
+
+isPurple('purple');         //true
+isPurple('purpleasd');         //false
+'pink'.toLowerCase() === 'purple';        //false
+'PurPLE'.toLowerCase() === 'purple';        //true
+
+
+
+
+
+
+function containsPurple(arr) {
+  for(let color of arr) {
+    if(color === 'purple' || color === 'magenta') {
+      return true;
+    }
+    return false
+  }
+}
+
+containsPurple(['blue', 'pink', 'magenta']);      //true
+containsPurple(['blue', 'pink', 'magentaasd']);        //false
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
