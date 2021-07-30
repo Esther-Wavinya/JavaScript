e4e042ec56b3d92263828a85869b40f53c9323c4
Esther Wavinya <estherwavinya80@gmail.com>
2021-07-30 21:18:19 +0300

    reduce intro

--- 09_10_Reduce_Intro/app.js
+++ 09_10_Reduce_Intro/app.js
@@ -1,3 +1,24 @@
+//Reduce - Executes a reducer function on each element of the array, resulting in a single value.
+
+
+
+//SUMMING AN ARRAY
+[3, 5, 7, 9, 11].reduce((accumulator, currentValue) => {
+  return accumulator + currentValue;
+});
+
+// callback         accumulator     currentValue      returnValue
+// first call            3            5                    8
+// second call           8            7                    15
+// third call            15           9                    24
+// fourth call           24          11                    35
+
+//35
+
+
+
+
+
 const nums = [3, 4, 5, 6, 7];
 // To multiply all values in nums:
 const product = nums.reduce((total, currentVal) => {
