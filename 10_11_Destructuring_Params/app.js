20322e58c72e2aa954e60cd53c19a4ed8b762077
Esther Wavinya <estherwavinya80@gmail.com>
2021-07-31 16:27:09 +0300

    destructuring params

---
+++ 10_11_Destructuring_Params/app.js
@@ -0,0 +1,48 @@
+const runner = {
+  first: "Eliud",
+  last: "Kipchoge",
+  country: "Kenya",
+  title: "Elder of the Order of the Golden Heart of Kenya"
+}
+
+
+
+// Rather than destructuring INSIDE the function body
+ function print(person) {
+  const {
+     first,
+     last,
+      title
+   } = person;
+   console.log(`${first} ${last}, ${title}`)
+ }
+
+
+
+// We can unpack(destructure) the values we want right in the parameter list:
+function print({
+  first,
+  last,
+  title
+}) {
+  console.log(`${first} ${last}, ${title}`)
+}
+
+
+
+
+
+//with arrays
+const response = [
+  'HTTP/1.1',
+  '200 OK',
+  'application/json',
+]
+
+// Also works with array parameters:
+function parseResponse([protocol, statusCode, contentType]) {
+  console.log(`Status: ${statusCode}`)
+}      
+parseResponse(response);         //Status: 200 OK
+
+
