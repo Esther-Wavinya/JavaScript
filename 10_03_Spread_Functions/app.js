1f71073d97f61d67f4be2580c6a3dfb3199bfdb7
Esther Wavinya <estherwavinya80@gmail.com>
2021-07-31 11:43:05 +0300

    spread functions

--- 10_03_Spread_Functions/app.js
+++ 10_03_Spread_Functions/app.js
@@ -1,3 +1,42 @@
+//SPREAD syntax allows an iterable such as an array to be expanded in places where zero or more arguments (for function calls) or elements (for array literals) are expected, or an object expression to be expanded in places where zero or more key value pairs(for object literals) are expected.
+
+
+
+//spread for function calls
+const nums = [ 9, 3, 2, 8 ];
+Math.max(nums);     //NaN
+//Use spread!
+Math.max(...nums);    //67
+//same as calling;
+//Math.max(9,3,2,8)    expands an iterable (array, string, etc. into a list of arguments)
+
+
+//spread in array literals
+//Creates a new array using an existing array. Spreads the elements from one array into a new array.
+const nums1 = [ 1, 2, 3 ];
+const nums2 = [ 4, 5, 6];
+[ ...nums1, ...nums2 ];     //[1, 2, 3, 4, 5, 6]
+
+[ 'a', 'b', ...nums2 ];    //["a", "b", 4, 5, 6]
+
+[ ...nums1, ...nums2, 7, 8, 9 ];   //[1, 2, 3, 4, 5, 6, 7, 8, 9 ]
+
+
+//spread in object literals
+//Copies properties from one object into another object literal
+const feline = { legs: 4, family: 'felidae' };
+const canline = { family: 'Canline', furry: true};
+
+const dog = { ...canline, isPet: true };
+
+
+const lion = { ...feline, genius: 'Panthera' };
+
+
+
+
+
+
 function giveMeFour(a, b, c, d) {
   console.log('a', a)
   console.log('b', b)
