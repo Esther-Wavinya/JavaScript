1b279944c770d98b0cb71865d2f9746aa450c08f
Esther Wavinya <estherwavinya80@gmail.com>
2021-07-31 12:22:48 +0300

    spread objects

--- 10_05_Spread_Objects/app.js
+++ 10_05_Spread_Objects/app.js
@@ -1,3 +1,17 @@
+//spread in object literals
+//Copies properties from one object into another object literal
+const feline = { legs: 4, family: 'Felidae' };
+const canine = { family: 'Caninae', furry: true};
+
+const dog = { ...canine, isPet: true };   //{family: "Caninae", furry: true, isPet: true }
+
+
+const lion = { ...feline, genius: 'Panthera' };    //{legs: 4, family: "Felidae", genius: "Panthera" }
+
+const catDog = { ...feline, ...canine };    //{legs: 4, family: "Caninae", furry: true }
+
+
+
 const feline = {
   legs: 4,
   family: 'Felidae'
@@ -23,11 +37,15 @@ const houseCat = {
 }
 //{legs: 4, family: "Felidae", isGrumpy: true, personality: "unpredictable"}
 
+
+//properties with the same key - legs: 4
 const catDog = {
   ...canine,
   ...feline
 }
 //{family: "Felidae", furry: true, legs: 4}
+
+
 
 //Order matters! Legs will be 3 here, because we set it AFTER spreading canine.
 const tripod = {
@@ -36,10 +54,28 @@ const tripod = {
 }
 //{family: "Caninae", furry: true, legs: 3}
 
+
+
+//copy
 const catDogClone = {
   ...catDog
-}
+}        
+//{family: "Felidae", furry: true, legs: 4}
 
+
+
+//spread array in an object literal
 const random = [...'hello', {
   ...catDog
 }]
+/*
+0: "h"
+1: "e"
+2: "l"
+3: "l"
+4: "o"
+5: 
+   family: "Felidae"
+   furry: true
+   legs: 4
+*/
