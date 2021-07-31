7f25054b2220f0326183787aae35fffca705a8da
Esther Wavinya <estherwavinya80@gmail.com>
2021-07-31 11:19:15 +0300

    spread parameter

--- 10_02_Default_Params/app.js
+++ 10_02_Default_Params/app.js
@@ -6,11 +6,14 @@
 //   return x * y;
 // }
 
+
 // A slightly shorter version:
 // function multiply(x, y) {
 //   y = typeof y === 'undefined' ? 1 : y
 //   return x * y;
 // }
+
+
 
 // The new super clean way of adding defaults!
 function multiply(x, y = 1) {
@@ -19,17 +22,23 @@ function multiply(x, y = 1) {
 multiply(3, 4); //12
 multiply(3); //3
 
+
+
+
 // Another example!
 // const greet = (person, greeting = 'hi') => {
 //   console.log(`${greeting}, ${person}!`)
 // }
+
+
 
 // Default value of an array:
 // const blah = (x, y = [1, 2, 3]) => {
 //   console.log(x, y);
 // }
 
-// Multiple default values are possible, but rare
+
+// Multiple default values are possible, but rare. default params
 // const greet = (person, greeting = 'hi', punctuation = '!') => {
 //   console.log(`${greeting}, ${person} ${punctuation}`)
 // }
