e4948eedd72c12d65c88334e0f4261356c03f744
Esther Wavinya <estherwavinya80@gmail.com>
2021-08-03 14:13:17 +0300

    shorthand method syntax on functions as properties on objects

--- 11_04_Method_Shorthand/app.js
+++ 11_04_Method_Shorthand/app.js
@@ -1,8 +1,24 @@
+//We do this so often that there's a new shorthand way of adding methods. SHORTHAND
+
+const math = {
+  blah: 'Hi!',
+  add(x, y) {
+    return x + y;
+  },
+  multiply(x, y) {
+    return x * y;
+  }
+}
+math.add(50, 60)   //110
+
+
+
+//A lot shorter
 const auth = {
   username: 'TommyBot',
   login() {
     console.log("LOGGED YOU IN!")
-  },
+  },   //Separate our properties by commas. it's a must
   logout() {
     console.log("GOODBYE")
   }
