b6af5d963fdce3ce52984599739b96b2abc2fbc4
Esther Wavinya <estherwavinya80@gmail.com>
2021-08-03 13:51:40 +0300

    computed properties

--- 11_02_Computed_Properties/app.js
+++ 11_02_Computed_Properties/app.js
@@ -10,35 +10,69 @@ const person2 = 'James Cameron'
 // Make the object
 // const team = {};
 // Then add a property using dynamic key:
-// team[role] = person;
-// team[role2] = person2;
+team[role] = person;
+team[role2] = person2;
+//{host: "Jools Holland", Director: "James Cameron"} 
 
 
 
 // USING COMPUTED PROPERTIES!
+//We can use a variable as a key name in an object literal property!
+const user = 'Jools';
+const userRoles = {
+  [user]: 'Admin'
+}
+userRoles;       //{Jools: "Admin"}
+
+
+
+//USING COMPUTED PROPERTIES
+const team = {
+  [role]: person,
+  [role2]: person2,
+}
+team;    //{host: "Jools Holland", Director: "James Cameron"} 
+
+//
 const team = {
   [role]: person,
   [role2]: person2,
   [1 + 6 + 9]: 'sixteen'
 }
+team;    //{16: "sixteen", host: "Jools Holland", Director: "James Cameron"}
 
-// function addProp(obj, k, v) {
-//   const copy = {
-//     ...obj
-//   };
-//   copy[k] = v;
-//   return copy;
-// }
 
-// const addProp = (obj, k, v) => {
-//   return {
-//     ...obj,
-//     [k]: v
-//   }
-// }
+
+
+
+//add a property to return a new function based on the obj with a key and value added
+//WITHOUT COMPUETED PROPERTY
+function addProp(obj, k, v) {
+const copy = {
+    ...obj
+  };
+  copy[k] = v;
+  return copy;
+}
+const res = addProp(team, 'happy', ':)')
+res;  //{16: "sixteen", host: "Jools Holland", Director: "James Cameron", happy: ":)"}
+
+
+//COMPUTED PROPERTY WITH ARROW FUNCTION
+const addProp = (obj, k, v) => {
+   return {
+     ...obj,
+     [k]: v
+   }
+ }
+ const res = addProp(team, 'happy', ':)') //{16: "sixteen", host: "Jools Holland", Director: "James Cameron", happy: ":)"}
+
+
+
+ //COMPUTED PROPERTY WITH IMPLICIT ARROW FUNCTION
 
 const addProp = (obj, k, v) => ({
   ...obj,
   [k]: v
 })
-const res = addProp(team, 'happy', ':)')
+const res = addProp(team, 'happy', ':)')  //{16: "sixteen", host: "Jools Holland", Director: "James Cameron", happy: ":)"}
