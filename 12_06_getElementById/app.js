621fa400689012c944fc715e6adfed08ebecf5e8
Esther Wavinya <estherwavinya80@gmail.com>
2021-08-05 14:48:55 +0300

    getelement by id

---
+++ 12_06_getElementById/app.js
@@ -0,0 +1,9 @@
+//The DOM is a JavaScript representation of a webpage
+//It is your JS "window into the contents of a webpage"
+//It is just a bunch of objects that you can interact with via JS
+
+// To select the element with id of bear-photo:
+document.getElementById('bear-photo')
+
+//To select the element with id of main:
+document.getElementById('main')
