b386f75008e3f87207cdf1fb5865cef621a9e296
Esther Wavinya <estherwavinya80@gmail.com>
2021-08-06 15:36:09 +0300

    queryselector & queryselectorAll

---
+++ 12_09_querySelector/app.js
@@ -0,0 +1,28 @@
+// ****************
+// querySelector - 
+//All-in-one method to select a single element for everything
+//pass in CSS Style selector
+// ****************
+//To find the first li on the page:
+document.querySelector('li');
+
+//To find the first element with class of special:
+document.querySelector('.special');
+
+//To find the first element with id of main (there should only be one...)
+document.querySelector('#main');
+
+// To find the first li with the class of special, nested inside of a ul, nested inside a section:
+document.querySelector('section ul li.special');
+
+
+// ****************
+// querySelectorAll -
+//Returns a collection.
+// ****************
+
+// To find ALL li's on the page:
+document.querySelectorAll('li');
+
+// To find ALL elements with the class of special on the page:
+document.querySelectorAll('.special');
