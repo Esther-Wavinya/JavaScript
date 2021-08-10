7b75570700198b832f852bd69f91bb2d3f6812d5
Esther Wavinya <estherwavinya80@gmail.com>
2021-08-10 12:55:18 +0300

    annoying button demo - event listeners

---
+++ 14_04_Annoying_Button_Demo/app.js
@@ -0,0 +1,17 @@
+//
+
+//select the button and store to a variable
+const btn = document.querySelector('button');
+//addeventlistener
+btn.addEventListener('mouseover', function() {
+	console.log('MOUSED OVER ME!');
+	const height = Math.floor(Math.random() * window.innerHeight);
+	const width = Math.floor(Math.random() * window.innerWidth);
+	btn.style.left = `${width}px`;
+	btn.style.top = `${height}px`;
+});
+
+btn.addEventListener('click', function() {
+	btn.innerText = 'YOU GOT ME!';
+	document.body.style.backgroundColor = 'green';
+});
