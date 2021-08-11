9d95e29e5f7abdf20856cd2e3049c59a28b0990c
Esther Wavinya <estherwavinya80@gmail.com>
2021-08-11 10:30:25 +0300

    creating promises

---
+++ 15_06_Creating_Promises/app.js
@@ -0,0 +1,21 @@
+//A promise is an object representing the eventual completion or failure of an asynchronous operation.
+//PROMISES - A PATTERN for writing async code
+//RESOLVE    REJECT
+//A promise is a returned object to which you attach callbacks, instead of passing callbacks into a function
+
+
+const willGetYouADog = new Promise((resolve, reject) => {
+	const rand = Math.random();
+	if (rand < 0.5) {
+		resolve();
+	}
+	else {
+		reject();
+	}
+});
+willGetYouADog.then(() => {
+	console.log('YAY WE GOT A DOG!!!!');
+});
+willGetYouADog.catch(() => {
+	console.log(':( NO DOG');
+});
