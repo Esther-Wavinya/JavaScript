53c1d50af024278ed2edc7a4de0db7d447747633
Esther Wavinya <estherwavinya80@gmail.com>
2021-08-12 11:36:57 +0300

    returning promises from a function

---
+++ 15_07_Returning_Promises/app.js
@@ -0,0 +1,24 @@
+//Returning promises from a function
+
+
+//Define a function that returns a promise
+const makeDogPromise = () => {
+	return new Promise((resolve, reject) => {
+		setTimeout(() => {
+			const rand = Math.random();
+			if (rand < 0.5) {
+				resolve();
+			}
+			else {
+				reject();
+			}
+		}, 5000);
+	});
+};
+makeDogPromise()
+	.then(() => {
+		console.log('YAY WE GOT A DOG!!!!');
+	})
+	.catch(() => {
+		console.log(':( NO DOG');
+	});
